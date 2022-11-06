6bb0e3d2e998f7ee92ff8566e6b80ee4c9ade85e
Jacob Flint <jacob.flint@outlook.com>
2022-11-06 23:25:35 +0000

    Adding objects to scenes

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def initMenuScene():
 	menuScene.orbit_radius:int = 200
 	menuScene.planet_pos = Vector2(screensize[0]/2, screensize[1]/2)
 	menuScene.on_update = game.menuScene_on_update
+	menuScene.storage = {'planet_orbit':"sun","orbit_transfer":lambda a:Vector2(0,0),"transfer_time":0,"in_transfer":False,"waiting":False,"wanted_angle":0,"angle_reset":False}
 
 	shuttle = Object("shuttle",Vector2(0,0),rotation=0, size=Vector2(50,50))
 	shuttle.addComponent(Components.Image(assetsManager.addAsset(Asset.ImageAsset("assets/images/player.bmp"))),assetsManager)
@@ -48,9 +49,13 @@ def initMenuScene():
 	planets_data = Asset.JSONAsset("assets/config/menu_planets.json")
 	planets_data:dict[str,dict] = planets_data()
 	planets = []
-	def planet_click_callback(planet_name:str):
+	def planet_click_callback(planet_name:str, window:engine.Window):
+		#print("pcc", planet_name)
 		if planet_name == "mars":
-			window.sceneManager.loadScene("1v1Scene")
+			window.sceneManager.reset_scene("1v1Scene")
+			s = init1v1Scene()
+			window.load_scene("1v1Scene")
+			
 	for name, data in planets_data.items():
 		planetObj = game.Planet(name, data)
 		planetObj.bind_image(assetsManager(Asset.ImageAsset(data["image"])))
@@ -60,14 +65,15 @@ def initMenuScene():
 			data["position"][0][1] + (data["position"][1][1] * screensize[1])
 			)
 		planetGObj = planetObj.createGameObject(window.dimensions, assetsManager, 0)
-		def callback():
-			planet_click_callback(name)
+		def callback(name, window, position, button, touch, *args, **kwargs):
+			planet_click_callback(name, window)
+			createPrintCallback(name).__call__()
 		window.input_manager.add_clickable_object("MenuScene", name)
-		window.input_manager.add_clickable_callback("MenuScene", name, callback)
-		planetGObj.addComponent(Components.Clickable(True))
+		window.input_manager.add_clickable_callback("MenuScene", name, copy_func(callback))
+		planetGObj.addComponent(Components.Clickable(True, CircleCollider()))
 		menuScene.addObject(planetGObj)
 
-		textObj = Object(name+"text",planetObj.position + Vector2(0,50*(1 if data["position"][1][1] <= 0.5 else -1)), 0, Vector2(200,50))
+		textObj = Object(name+"text",planetObj.position + Vector2(0,100*(-1 if data["position"][1][1] <= 0.5 else 1)), 0, Vector2(0,0))
 		textObj.addComponent(Components.Text(data["text"],Vector3(230,230,230), assetsManager(Asset.TextAsset("assets/fonts/ROBOTO-THIN.TTF",32))), assetsManager)
 		menuScene.addObject(textObj)
 
@@ -104,21 +110,29 @@ def initSingleScene():
 	for i in range(3):
 		surf = transform.rotate(surf, 30)
 		surfaces.append(surf)
-	gameScene.add_particle_emitter("engine",ParticleEmitter(tuple(surfaces)))
+	gameScene.add_particle_emitter("engine",ParticleEmitter("engine", tuple(surfaces)))
 
 	return gameScene
 
 def init1v1Scene():
-	global player1, player2, planets
+	global player1, player2, planets, player1hb, player2hb
 	gameScene = Scene("1v1Scene", background_color=Vector3(42,52,64))
+	gameScene.storage['reset_timer'] = None
 
 	player1 = game.Player(assetsManager(Asset.ImageAsset("assets/images/player.bmp")),1)
+	player1hb = game.HealthBar(3, Vector2(10,10))
+	player1.link_health_bar(player1hb)
 	player1_obj = player1.createGameObject(assetsManager)
 	gameScene.addObject(player1_obj)
+	gameScene.storage.update({"player1hb" : player1hb})
 
 	player2 = game.Player(assetsManager(Asset.ImageAsset("assets/images/player.bmp")),2)
+	player2.position = Vector2(*window.dimensions) - player1.position
+	player2hb = game.HealthBar(3, Vector2(window.dimensions[0]-200-10, 10))
+	player2.link_health_bar(player2hb)
 	player2_obj = player2.createGameObject(assetsManager)
 	gameScene.addObject(player2_obj)
+	gameScene.storage.update({'player2hb' : player2hb})
 
 	planets_data = Asset.JSONAsset("assets/config/planets.json")
 	planets_data:dict[str,dict] = planets_data()
@@ -144,10 +158,35 @@ def init1v1Scene():
 	for i in range(3):
 		surf = transform.rotate(surf, 30)
 		surfaces.append(surf)
-	gameScene.add_particle_emitter("engine",ParticleEmitter(tuple(surfaces)))
+	surf = transform.scale(image.load("assets/images/particle2.bmp"), (2,2))
+	surfaces.append(surf)
+	for i in range(3):
+		surf = transform.rotate(surf, 30)
+		surfaces.append(surf)
+	gameScene.add_particle_emitter("engine1",ParticleEmitter("engine1", tuple(surfaces)))
+	gameScene.add_particle_emitter("engine2",ParticleEmitter("engine2", tuple(surfaces)))
+
+	gameScene.add_particle_emitter("exp1",ParticleEmitter("engine1", tuple(surfaces)))
+	gameScene.add_particle_emitter("exp2",ParticleEmitter("engine2", tuple(surfaces)))
 
 	surf = transform.scale(image.load("assets/images/particle3.bmp"), (4,4))
-	gameScene.add_particle_emitter("turret",ParticleEmitter((surf,None),single=True))
+	gameScene.add_particle_emitter("turret1",ParticleEmitter("turret1", (surf,None),single=True, callback=game.bullet_collision_callback))
+	gameScene.add_particle_emitter("turret2",ParticleEmitter("turret2", (surf,None),single=True, callback=game.bullet_collision_callback))
+
+	msgBox = Object("msgBox",Vector2(*window.dimensions)/4,size=Vector2(window.dimensions[0]/2,window.dimensions[1]/2))
+	msgBox.addComponent(Components.Primitive(Constants.PRIMITIVE_RECTANGLE, (15,15,15,200)))
+	msgBox.primitive.should_render = False
+	gameScene.addObject(msgBox)
+
+	endMessage = Object("endMessage",Vector2(*window.dimensions)/2,size=Vector2(window.dimensions[0]/2, window.dimensions[1]/2))
+	endMessage.addComponent(Components.Text("msg",Vector3(225,220,220),assetsManager(Asset.TextAsset("assets/fonts/ROBOTO-THIN.TTF",48))),assetsManager)
+	endMessage.text.should_render = False
+	gameScene.addObject(endMessage)
+
+	gameScene.on_update = game.on_1v1Scene_update
+
+	gameScene.on_render = game.on_render_1v1Scene
+	gameScene.object_collision_callback = game.on_object_collision
 
 	return gameScene
 
