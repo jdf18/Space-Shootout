4cba98b5b8bfe88cdcc82bd70c9f9bb2895f7ca4
Jacob Flint <jacob.flint@outlook.com>
2022-11-06 23:18:45 +0000

    Fixing all the problems and QOL

--- engine/objects.py
+++ engine/objects.py
@@ -4,6 +4,7 @@ from pygame import Surface, Vector2, Vector3, Rect, image
 from engine.assetsManager import Asset, AssetsManager
 from engine.constants import Constants
 from pygame.font import Font
+from engine.colliders import Collider, CircleCollider
 
 class Components:
 	class Component:
@@ -18,7 +19,7 @@ class Components:
 		def update(self, dt:float, object): pass
 		@abstractmethod
 		def __repr__(self) -> str: return ""
-	class RComponent:
+	class RComponent(Component):
 		RENDER:bool=True
 		@abstractmethod
 		def render(self, pygame): pass
@@ -33,6 +34,7 @@ class Components:
 			self.velocity:Vector2 = Vector2(0,0)
 			self.force:Vector2 = Vector2(0,0)
 			self.rect:Rect = Rect(self.position,self.size)
+			self.center:Vector2 = self.position + (self.size/2)
 		def update(self, dt:float, object):
 			self.velocity += (self.force*dt)
 			self.position += (self.velocity*dt)
@@ -41,6 +43,7 @@ class Components:
 			self.rotation %= 360
 		def __repr__(self) -> str:
 			return f"Position: {self.position.__repr__()}\nRotation: {self.rotation.__repr__()}\nSize: {self.size.__repr__()}"
+
 	class Primitive(RComponent):
 		INDEX=1
 		NAME="primitive"
@@ -49,6 +52,7 @@ class Components:
 			assert type in LIST_OF_TYPES, "ERROR: " + str(type) + " is not a supported primitive type."
 			self.type:int = type
 			self.color:Vector3 = color
+			self.should_render = True
 		def init(self, object, assetsManager: AssetsManager):
 			self.position:tuple = tuple(object.transform.position)
 			self.size:tuple = tuple(object.transform.size)
@@ -58,6 +62,7 @@ class Components:
 			self.size:Vector2 = object.transform.size
 			self.rect:Rect = Rect(self.position,self.size)
 		def render(self, pygame):
+			if not self.should_render: return
 			window = pygame.display.get_surface()
 			if self.type == Constants.PRIMITIVE_RECTANGLE:
 				pygame.draw.rect(window, tuple(self.color), tuple(self.rect))
@@ -65,6 +70,7 @@ class Components:
 				pygame.draw.circle(window, self.color,
 					self.position + (self.size/2),
 					(self.size/2).magnitude())
+
 	class Image(RComponent):
 		INDEX:int=2
 		NAME:str="image"
@@ -73,6 +79,9 @@ class Components:
 			self.img = None
 			self.img_array = None
 			self.shader = None
+			self.position = Vector2(0,0)
+			self.rotation = 0
+			self.should_render = True
 		def init(self, object, assetManager: AssetsManager):
 			asset:Asset.ImageAsset = assetManager.getAsset(self.image_asset_id)
 			self.img = asset.img
@@ -82,6 +91,7 @@ class Components:
 			self.position = tuple(object.transform.position)
 			self.rotation = object.transform.rotation
 		def render(self, pygame):
+			if not self.should_render: return
 			if not self.shader: output_array = self.img_array
 			else: output_array = self.shader(self.img_array)
 			surface = image.frombuffer(output_array.tobytes(), self.img.size, self.img.mode)
@@ -93,28 +103,37 @@ class Components:
 			else:
 				offset = Vector2(0,0)
 			pygame.display.get_surface().blit(scale_surf, self.position+offset)
+
 	class Text(RComponent):
 		INDEX:int=3
 		NAME:str="text"
 		def __init__(self, text:str, color:Vector3, font_asset_id:int):
 			self.font_asset_id:int = font_asset_id
 			self.text:str = text
-			self.last_text:str = not text
+			self.last_text:str = None
 			self.color:Vector3 = color
 			self.dimensions:Vector2 = None
+			self.should_render = True
+			self.surface = None
+			self.rotation:int = 0
+			self.position = Vector2(0,0)
 		def init(self, object, assetManager: AssetsManager):
 			asset:Asset.FontAsset = assetManager.getAsset(self.font_asset_id)
 			self.font:Font = asset.font
 			self.dimensions:Vector2 = object.transform.size
 		def update(self, dt:float, object):
-			if self.last_text != self.text:
+			if self.last_text != self.text or self.last_text == None:
 				self.surface:Surface = self.font.render(self.text, True, self.color)
 				self.dimensions:Vector2 = Vector2(*self.surface.get_size())
 				self.last_text:str = self.text
 			self.position = Vector2(object.transform.position)
 			self.rotation:float = object.transform.rotation
 		def render(self, pygame):
-			scale_surf:Surface = self.surface # pygame.transform.scale(surface,self.dimensions)
+			if not self.should_render: return
+			if not self.surface: 
+				self.surface:Surface = self.font.render(self.text, True, self.color)
+				self.dimensions:Vector2 = Vector2(*self.surface.get_size())
+			scale_surf:Surface = self.surface
 			if self.rotation != 0:
 				offset:Vector2 = self.dimensions/2
 				scale_surf = pygame.transform.rotate(scale_surf,self.rotation)
@@ -122,23 +141,39 @@ class Components:
 			else:
 				offset = Vector2(0,0)
 			pygame.display.get_surface().blit(scale_surf, self.position+offset-self.dimensions/2)
+		def __repr__(self):
+			return f"Text: {self.text}, {self.color}"
+
+
 	class Clickable(Component):
 		INDEX:int=4
 		NAME:str="clickable"
-		def __init__(self, is_static:bool):
+		def __init__(self, is_static:bool, collider:Collider):
 			self.static:bool = is_static
+			self.collider = collider
+		def update(self, dt:float, object):
+			self.collider.update(object)
+		def test_pos(self, position:Vector2) -> bool:
+			return self.collider.test(position)
+
 
 class Object:
 	transform:Components.Transform
 	primitive:Components.Primitive
 	image:Components.Image
-	def __init__(self, name:str, position:Vector2, rotation:float=0, size:Vector2=Vector2(1,1)):
+	text:Components.Text
+	clickable:Components.Clickable
+	collider:Collider
+	def __init__(self, name:str, position:Vector2, rotation:float=0, size:Vector2=Vector2(1,1), tags:list[str]=["none"]):
 		self.name:str = name
 		self.added_components:list[str] = ["transform"]
 		self.render_components:list[str] = []
 		self.transform = Components.Transform(position, rotation, size)
 		self.should_render:bool = False
+		self.collider:Collider = Collider()
+		self.tags:list[str] = tags
 	def update(self, dt:float):
+		self.collider.update(self)
 		for component in self.added_components:
 			self.__getattribute__(component).update(dt, self)
 	def addComponent(self, component: Components.Component, assetManager:AssetsManager=None):
@@ -153,7 +188,7 @@ class Object:
 		for component_name in self.render_components:
 			self.__getattribute__(component_name).render(pygame)
 	def __repr__(self):
-		out = f"Object: {self.name}\nShould render: {str(self.should_render)}\nComponents: {self.added_components}"
+		out = f"\nObject: {self.name}\nShould render: {str(self.should_render)}\nComponents: {self.added_components}\n"
 		for component in self.added_components:
 			out += self.__getattribute__(component).__repr__()
 		return out
