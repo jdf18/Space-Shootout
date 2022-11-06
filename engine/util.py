d9f3dd214b662ee584467ca5640bcfe13a0de5a7
Jacob Flint <jacob.flint@outlook.com>
2022-11-06 23:19:30 +0000

    More stuff ill probably need later

--- engine/util.py
+++ engine/util.py
@@ -1,4 +1,4 @@
-from time import time
+from time import time, sleep
 from typing import Callable
 from pygame.math import Vector2
 
@@ -17,3 +17,17 @@ def get_lerp(p1:Vector2,p2:Vector2) -> Callable:
 	def foo(val:float) -> Vector2:
 		return p1 + (val * (p2-p1))
 	return foo
+	
+def lerp(t:float, p1:Vector2,p2:Vector2) -> Vector2:
+	return p1 + (t * (p2-p1))
+	
+
+import types, functools
+
+def copy_func(f):
+    g = types.FunctionType(f.__code__, f.__globals__, name=f.__name__,
+                           argdefs=f.__defaults__,
+                           closure=f.__closure__)
+    g = functools.update_wrapper(g, f)
+    g.__kwdefaults__ = f.__kwdefaults__
+    return g
